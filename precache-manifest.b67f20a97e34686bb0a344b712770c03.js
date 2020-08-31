e96b8cefde2a67723c26108566cf8533f193e6b4
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-31 20:12:58 +0800

    Updates

---
+++ precache-manifest.b67f20a97e34686bb0a344b712770c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39719861576cc5004cf213b4ca6b9a32",
+    "url": "/react-search-movies/index.html"
+  },
+  {
+    "revision": "0e4fc522b8fcc0f49674",
+    "url": "/react-search-movies/static/css/main.25ea6951.chunk.css"
+  },
+  {
+    "revision": "acfbabf594039942dbab",
+    "url": "/react-search-movies/static/js/2.d3d3c1c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-search-movies/static/js/2.d3d3c1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4fc522b8fcc0f49674",
+    "url": "/react-search-movies/static/js/main.1057c305.chunk.js"
+  },
+  {
+    "revision": "a3dc1365eb93360bcee5",
+    "url": "/react-search-movies/static/js/runtime-main.4a3ae3a0.js"
+  }
+]);
