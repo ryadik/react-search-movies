901e7fcd52e1443c4a8c8e4c2d01903db690e168
K1nGsmaN-hub <15162342h@gmail.com>
2020-08-31 20:10:40 +0800

    Created state. Added getData function and import Search, Posters components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, {Component} from "react";
 
 import Header from "./components/header/header";
+import Search from "./components/search/search";
+import Posters from "./components/posters/posters";
 
 import './App.css'
+import {getMovie} from "./services/getMovie";
 
-const App = () => {
-  return (
-      <div className="wrapper">
-        <Header/>
-      </div>
-  )
+class App extends Component{
+  state = {
+    movie: [],
+    error: ''
+  }
+
+  getData = (text) => {
+    getMovie(text)
+        .then(res => {
+          if (res.Response === 'False') {
+            this.setState({
+              error: res.Error,
+              movie: []
+            })
+          } else {
+            this.setState({
+              movie: res.Search,
+              error: ''
+            })
+          }
+        })
+  }
+
+  render() {
+    return (
+        <div className="wrapper">
+          <Header/>
+          <Search getData={this.getData}/>
+          <Posters movie={this.state.movie} error={this.state.error}/>
+        </div>
+    )
+  }
+
 }
 export default App
